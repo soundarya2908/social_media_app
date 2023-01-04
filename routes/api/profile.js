8235a96bea7209b226bf561fc5c9d45bdcb5e3e3
soundarya <srikanta.s@northeastern.edu>
2023-01-04 15:05:29 -0800

    get profile by id and get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -69,4 +69,31 @@ router.post('/', [auth, [
 
 })
 
+router.get('/', async(req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+        res.json(profiles)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+})
+
+router.get('/user/:user_id', async(req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.params.user_id}).populate('user', ['name', 'avatar'])
+
+        if(!profile) {
+            return res.status(400).json({msg: 'Profile not found'})
+        }
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({msg: 'Profile not found'})
+        }
+        res.status(500).send('Server error')
+    }
+})
+
 module.exports = router;
